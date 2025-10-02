d8239f030ee68e4c24fff04a1023bf6d6aaacf94
Tech Programmer <143309368+tech011@users.noreply.github.com>
2025-10-02 14:47:35 +0530

    Update 6_message.py

--- 6_message.py
+++ 6_message.py
@@ -1,28 +1,19 @@
 import numpy as np
 import pandas as pd
+#import nltk
 import matplotlib.pyplot as plt
 
-airtweets = pd.read_csv("Tweets.csv")
-plt.rcParams["figure.figsize"] = (8, 6)
+data_source_url = "Tweets.csv"
+airtweets = pd.read_csv(data_source_url)
 
+plsize = plt.rcParams["figure.figsize"]
 print("Data in CSV file")
 print(airtweets.head())
 
-print("Available columns in the CSV:")
-print(airtweets.columns)
+print("Distribution of sentiments across all tweets ")
+airtweets.airline_sentiment.value_counts().plot(kind='pie', autopct='%1.0f%%', colors=["black", "orange", "green"])
 
-print("Distribution of sentiments across all tweets")
-airtweets['sentiment'].value_counts().plot(
-    kind='pie',
-    autopct='%1.0f%%',
-    colors=["black", "orange", "green"]
-)
+airline_sentiment = airtweets.groupby(['airline', 'airline_sentiment'])
+airline_sentiment = airline_sentiment.count().unstack()
 
-airline_sentiment = airtweets.groupby(['airline', 'sentiment'])['sentiment'].count().unstack()
-airline_sentiment.plot(
-    kind='bar',
-    color=['black', 'orange', 'green']
-)
-plt.title("Airline-wise Sentiment Count")
-plt.ylabel("Number of Tweets")
-plt.show()
+airline_sentiment.plot(kind='bar', color=['black', 'blue', 'cyan'])
